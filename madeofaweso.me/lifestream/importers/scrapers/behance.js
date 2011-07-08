a6ff9af8936df712e91852cab66d8c9403161003
Dan Drinkard <dan.drinkard@gmail.com>
2011-07-08 14:56:52 -0400

    woof

---
+++ madeofaweso.me/lifestream/importers/scrapers/behance.js
@@ -0,0 +1,12 @@
+var jquery = require('jquery'),
+    sys = require('sys');
+
+exports.run = function(window){
+    var $ = jquery;
+
+    var items = $('.cover-body', window);
+    items.each(function(){
+
+    });
+
+}
